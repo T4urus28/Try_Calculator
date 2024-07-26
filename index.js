d855db1b0ee2b52e866ae21cad1403fbf3801156
bimm <justbimm@gmail.com>
2024-07-26 16:22:33 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Calculator project
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+display.value += input;
+}
+
+function clearDisplay(){
+display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(eror){
+        display.value = "Minta digaplok"
+    }
+}
